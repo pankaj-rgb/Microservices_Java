5c552885da04a16128ceed26d83d5b0f2d464cde
Nanami <royalpankaj63@gmail.com>
2024-09-13 20:51:37 +0530

    Eureka Discovery Client with server and registry of the application.

--- order-service/src/main/java/com/pankajproduct/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/pankajproduct/order_service/OrderServiceApplication.java
@@ -3,7 +3,9 @@ package com.pankajproduct.order_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
--- product-service/src/main/java/com/pankajproduct/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/com/pankajproduct/product_service/ProductServiceApplication.java
@@ -3,7 +3,9 @@ package com.pankajproduct.product_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
